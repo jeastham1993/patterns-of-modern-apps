6e8636655210f1e1080929c9f18ac06116897979
james.eastham <james.eastham@datadoghq.com>
2024-10-09 10:19:58 +0100

    Update to add Kafka publisher

--- src/web/src/main.rs
+++ src/web/src/main.rs
@@ -1,21 +1,43 @@
+use std::{sync::Arc, time::Duration};
+
 use axum::{
-    routing::{get, post},
+    extract::State,
     http::StatusCode,
+    routing::{get, post},
     Json, Router,
 };
+use rdkafka::{
+    producer::{FutureProducer, FutureRecord},
+    ClientConfig,
+};
 use serde::{Deserialize, Serialize};
+
+pub struct AppState {
+    pub kafka_producer: Arc<FutureProducer>,
+}
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
+
+    let shared_state = Arc::new(AppState {
+        kafka_producer: Arc::new(producer),
+    });
+
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        .with_state(shared_state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -28,8 +50,7 @@ async fn root() -> &'static str {
 }
 
 async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
+    State(state): State<Arc<AppState>>,
     Json(payload): Json<CreateUser>,
 ) -> (StatusCode, Json<User>) {
     // insert your application logic here
@@ -37,6 +58,17 @@ async fn create_user(
         id: 1337,
         username: payload.username,
     };
+
+    let delivery_status = state.kafka_producer
+        .send(
+            FutureRecord::to("rust-kafka")
+                .payload("world")
+                .key("hello"),
+            Duration::from_secs(0),
+        )
+        .await;
+
+    tracing::info!("Delivery status for message {} received", delivery_status.unwrap().0);
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
