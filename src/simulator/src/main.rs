aaaee5c64e66c8ab91cf86342a85ff5c23c19af1
james.eastham <james.eastham@datadoghq.com>
2024-10-25 18:06:45 +0100

    Update simulator

--- src/simulator/src/main.rs
+++ src/simulator/src/main.rs
@@ -4,6 +4,7 @@ use rdkafka::{
     ClientConfig,
 };
 use serde::Serialize;
+use uuid::Uuid;
 use std::time::Duration;
 use tokio::signal;
 use tracing::info;
@@ -15,40 +16,7 @@ pub struct OrderConfirmed {
     order_value: f32,
 }
 
-async fn process(producer: FutureProducer, topic: &str) {
-    loop {
-        info!("Producing");
-
-        let order_num = rand::thread_rng().gen_range(0..100);
-
-        let order_value_int = rand::thread_rng().gen_range(0..10000);
-        let order_value: f32 = order_value_int as f32;
-
-        let data = OrderConfirmed {
-            customer_id: "james".to_string(),
-            order_id: format!("ORD{}", order_num),
-            order_value: order_value / 100.00,
-        };
-
-        let serialized = serde_json::to_string(&data).unwrap();
-
-        let _ = producer
-            .send(
-                FutureRecord::to(topic)
-                    .payload(&serialized)
-                    .key(&data.order_id),
-                Duration::from_secs(0),
-            )
-            .await;
-
-        std::thread::sleep(Duration::from_secs(1));
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt().init();
-
+async fn process() {
     let username = std::env::var("KAFKA_USERNAME");
     let password = std::env::var("KAFKA_PASSWORD");
     let broker = std::env::var("BROKER").expect("Broker should be set");
@@ -74,9 +42,52 @@ async fn main() {
             .expect("Producer creation failed"),
     };
 
+    loop {
+        let customer_under_test = format!("test_{}", Uuid::new_v4());
+        info!("Producing for {}", customer_under_test);
+
+        let order_num = rand::thread_rng().gen_range(0..100);
+
+        let order_value_int = rand::thread_rng().gen_range(0..10000);
+        let order_value: f32 = order_value_int as f32;
+
+        let data = OrderConfirmed {
+            customer_id: customer_under_test,
+            order_id: format!("ORD{}", order_num),
+            order_value: order_value / 100.00,
+        };
+
+        let serialized = serde_json::to_string(&data).unwrap();
+
+        let _ = producer
+            .send(
+                FutureRecord::to("order-completed")
+                    .payload(&serialized)
+                    .key(&data.order_id),
+                Duration::from_secs(0),
+            )
+            .await;
+
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
     tokio::spawn(async move {
-        process(producer, "order-completed").await;
+        process().await;
     });
+    // tokio::spawn(async move {
+    //     process().await;
+    // });
+    // tokio::spawn(async move {
+    //     process().await;
+    // });
+    // tokio::spawn(async move {
+    //     process().await;
+    // });
 
     match signal::ctrl_c().await {
         Ok(()) => {
